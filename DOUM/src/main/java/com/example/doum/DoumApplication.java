c758621daa5d0bcc44f5ea5e1972e0e7e6076f99
wongwangho <dkdkdkdk2086@naver.com>
2024-07-05 21:15:31 +0900

    start

---
+++ DOUM/src/main/java/com/example/doum/DoumApplication.java
@@ -0,0 +1,13 @@
+package com.example.doum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoumApplication.class, args);
+    }
+
+}
