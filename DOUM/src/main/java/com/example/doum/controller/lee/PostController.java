08ff0cb07d0539584dd5dc2610446516d79edb25
stella9921 <stella9921@naver.com>
2024-07-09 05:39:35 +0900

    이현수post부분 작성

--- DOUM/src/main/java/com/example/doum/controller/lee/PostController.java
+++ DOUM/src/main/java/com/example/doum/controller/lee/PostController.java
@@ -1,10 +1,14 @@
 package com.example.doum.controller.lee;
 
 
+import ch.qos.logback.core.model.Model;
+import com.example.doum.domain.dto.lee.PostDTO;
 import com.example.doum.mapper.lee.UsersMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,7 +21,20 @@ public class PostController {
     private final UsersMapper usersMapper;
 
 
-    @GetMapping("/list")
+    //마이페이지 봉사작성글 폼으로 이동
+    @GetMapping("/write")
+    public String writeForm(Model model) {
+        model.addAttribute("post", new PostDTO());
+        return "post/write";
+    }
+
+
+    //마이페이지 봉사작성글 처리
+    @PostMapping("/write")
+    public String write(PostDTO post, Model model) {
+
+
+    }
 
 
 }
